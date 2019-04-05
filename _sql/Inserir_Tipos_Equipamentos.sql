3afd97c7d0142fa94379562fc700562bee9b72cc
Isaque Marinho Ribeiro <isaque.ribeiro@outlook.com>
2019-04-04 23:51:38 -0300

    Inserção de novos artefatos

---
+++ _sql/Inserir_Tipos_Equipamentos.sql
@@ -0,0 +1,18 @@
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (0, '* Indefinido', 'IND');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (1, 'Carro', 'CAR');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (2, 'Moto', 'MOT');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (3, 'Computador', 'COM');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (4, 'Impressora', 'IMP');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (5, 'Periféricos', 'PRF');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (6, 'Peças em Geral', 'PEG');
+INSERT INTO SYS_TIPO_EQUIPAMENTO (CODIGO, DESCRICAO, SIGLA)
+                          VALUES (99, 'Outros', 'OUT');
+
+COMMIT WORK;
